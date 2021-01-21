36e52676e41694b63b4f1a00887b112af0c9df8e
Kimbangul <highcolor_12@naver.com>
2021-01-21 22:30:20 +0900

    슬라이드 작업

--- js/script.js
+++ js/script.js
@@ -56,33 +56,40 @@ $(document).ready(function(){
     }
 
     function mainSlide_init(){
-        $('div#slider').slick({
-            slide: 'div',
-            infinite: true,
-            slidesToShow:1,
-            arrows : true,
-            speed: 700,
-            autoplay : true,
-            autoplaySpeed: 5000,
-            dots: true,
-            prevArrow : "<button type='button' class='slick-prev'>Previous</button>",		// 이전 화살표 모양 설정
-            nextArrow : "<button type='button' class='slick-next'>Next</button>",
-            dotsClass: "dots",
-            draggable: true
+        function mainSlide(){
+            $('div#slider').slick({
+                slide: 'div',
+                infinite: true,
+                slidesToShow:1,
+                arrows : true,
+                speed: 700,
+                autoplay : true,
+                autoplaySpeed: 5000,
+                dots: true,
+                prevArrow : "<button type='button' class='slick-prev'>Previous</button>",		// 이전 화살표 모양 설정
+                nextArrow : "<button type='button' class='slick-next'>Next</button>",
+                dotsClass: "dots",
+                draggable: true
+    
+            });
 
-        });
+        }
+
 
         function slideBgAdd(){
-            $targetSlide = $('section#slide > div#slider > div');
-            $slideNum = $targetSlide.data('slidenum');
-            $slideImgSrc = $targetSlide.find('img').attr('src');
-            console.log($slideImgSrc);
-            console.log($slideNum);
-            
-            $targetSlide.css({"background":"url("+ $slideImgSrc + ")"})
+            var $targetSlide = $('section#slide > div#slider div.slide-box').has('img');            
+
+                $targetSlide.each(function(){
+                    var $slideImgSrc = $(this).find('img').attr('src');
+
+                    $(this).css(
+                        {"background":"url("+ $slideImgSrc + ")"}
+                        );
+                });                    
         }
 
         slideBgAdd();
+        mainSlide();
 
     }
 
